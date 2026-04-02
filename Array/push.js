a22072d60232eb1b35c8c856a8c0f4c0571c8945
Riya <sadhukhanriya55@gmail.com>
2026-04-02 16:45:31 +0530

    push.js implemented

--- Array/push.js
+++ Array/push.js
@@ -1,4 +1,18 @@
-let arr = [1, 2, 3];
-arr.push(4);
+/*
+ * push 
+implementation
+*
+*/
 
-console.log(arr); // Output: [1, 2, 3, 4]
+let arr = [1, 2, 3];
+
+function push(...elements) {
+  for (let i = 0; i < elements.length; i++) {
+    arr[arr.length] = elements[i];
+    console.log("Updated array: " + arr);
+    console.log("current array length: " + arr.length);
+  }
+  console.log("Final array: " + arr);
+}
+
+push(5, 6);
