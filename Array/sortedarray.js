24ef31562b4b1e23e33b4dbacd88607256501fa4
Riya <sadhukhanriya55@gmail.com>
2026-04-07 21:00:07 +0530

    sortedarray implement

---
+++ Array/sortedarray.js
@@ -0,0 +1,13 @@
+/*check if array is sorted*/
+
+function isSorted(arr) {
+  for (let i = 0; i < arr.length - 1; i++) {
+    if (arr[i] > arr[i + 1]) {
+      return false;
+    }
+  }
+  return true;
+}
+
+console.log(isSorted([1, 2, 3, 4])); // true
+console.log(isSorted([1, 3, 2])); // false
