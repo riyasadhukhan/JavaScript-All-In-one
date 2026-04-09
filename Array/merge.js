ca248f748724e250a07a63f2f2ac0e8f5c2c19ee
Riya <sadhukhanriya55@gmail.com>
2026-04-09 12:00:01 +0530

    merged two sorted array

---
+++ Array/merge.js
@@ -0,0 +1,31 @@
+/*Merge two sorted array*/
+
+function mergeSorted(arr1, arr2) {
+  let result = [];
+  let i = 0;
+  let j = 0;
+  while (i < arr1.length && j < arr2.length) {
+    if (arr1[i] < arr2[j]) {
+      result.push(arr1[i]);
+      i++;
+    } else {
+      result.push(arr2[j]);
+      j++;
+    }
+  }
+
+  while (i < arr1.length) {
+    result.push(arr1[i]);
+    i++;
+  }
+
+  while (j < arr2.length) {
+    result.push(arr2[j]);
+    j++;
+  }
+
+  return result;
+}
+
+console.log(mergeSorted([1, 3, 7, 8], [2, 4, 6, 9, 10, 12]));
+// [1,2,3,4,5,6]
