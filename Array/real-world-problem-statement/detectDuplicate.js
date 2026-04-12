726b15aef00860aa55611fbd4b396f5c3e7b293b
Riya <sadhukhanriya55@gmail.com>
2026-04-12 20:08:06 +0530

    detect duplicate

---
+++ Array/real-world-problem-statement/detectDuplicate.js
@@ -0,0 +1,17 @@
+/* Return true if any number appears more than once. */
+
+const ids = [1, 2, 3, 4, 2];
+
+function detectDuplicate(arr) {
+  let isDuplicate = false;
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[i] === arr[j]) {
+        return true;
+      }
+    }
+  }
+  return false;
+}
+
+console.log(detectDuplicate(ids));
