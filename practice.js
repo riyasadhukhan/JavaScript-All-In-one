6528f54ca0500ebe5c01fee1e100edfab58d5366
Riya <sadhukhanriya55@gmail.com>
2026-04-17 14:26:10 +0530

    practice set 2

--- practice.js
+++ practice.js
@@ -13,7 +13,7 @@ function practice(users) {
 
 console.log(practice(users));*/
 
-const users = [
+/*const users = [
   { name: "Rahul", isActive: true },
   { name: "Sneha", isActive: false },
   { name: "Arjun", isActive: true },
@@ -29,4 +29,37 @@ function practice(users) {
   return arr;
 }
 
-console.log(practice(users));
+console.log(practice(users));*/
+
+/*const cart = [
+  { product: "Shirt", quantity: 2 },
+  { product: "Shoes", quantity: 1 },
+];
+
+function practice(cart) {
+  let total = 0;
+  for (let item of cart) {
+    total += item.quantity;
+  }
+  return total;
+}
+
+console.log(practice(cart));*/
+
+const products = [
+  { name: "Watch", price: 1500 },
+  { name: "Pen", price: 50 },
+  { name: "Bag", price: 1200 },
+];
+
+function practice(products) {
+  let arr = [];
+  for (let product of products) {
+    if (product.price > 1000) {
+      arr.push(product);
+    }
+  }
+  return arr;
+}
+
+console.log(practice(products));
